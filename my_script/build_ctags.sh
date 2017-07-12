ae842fa124fddbd30f56ed44412f11be850cf636
zheng39562 <zheng39562@163.com>
2017-07-11 23:07:30 -0400

    1

--- my_script/build_ctags.sh
+++ my_script/build_ctags.sh
@@ -2,6 +2,6 @@
 
 /usr/bin/rm ~/base_include.tags
 touch ~/base_include.tags
-find /usr/include/ -iname '*.cc' -or -iname '*.c' -or -iname '*.cpp' -or -iname '*.hpp' -or -iname '*.h' | xargs ctags --c++-kinds=+p --fields=+iaS --extra=+q --langmap=c++:+.inl -a ~/base_include.tags
-find /usr/local/include/ -iname '*.cc' -or -iname '*.c' -or -iname '*.cpp' -or -iname '*.hpp' -or -iname '*.h' | xargs ctags --c++-kinds=+p --fields=+iaS --extra=+q --langmap=c++:+.inl -a ~/base_include.tags
+find /usr/include/ -iname '*.cc' -or -iname '*.c' -or -iname '*.cpp' -or -iname '*.hpp' -or -iname '*.h' | xargs ctags --c++-kinds=+p --fields=+iaS --extra=+q --langmap=c++:+.inl --append=yes -o ~/base_include.tags
+find /usr/local/include/ -iname '*.cc' -or -iname '*.c' -or -iname '*.cpp' -or -iname '*.hpp' -or -iname '*.h' | xargs ctags --c++-kinds=+p --fields=+iaS --extra=+q --langmap=c++:+.inl --append=yes -o ~/base_include.tags
 
