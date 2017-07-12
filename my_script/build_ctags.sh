08cd5a7414d0c5b27b15af95c1fa94b52da3b61c
zjm <zjm@192.168.31.128>
2017-07-11 23:05:17 -0400

    1

--- my_script/build_ctags.sh
+++ my_script/build_ctags.sh
@@ -1,12 +1,7 @@
 #! /bin/bash
 
-BASE_PATH=$1
-
-if [ ! -n ${BASE_PATH} ]; then
-	BASE_PATH="/usr/include/"
-fi
-
 /usr/bin/rm ~/base_include.tags
 touch ~/base_include.tags
-find ${BASE_PATH} -iname '*.c' -or -iname '*.cpp' -or -iname '*.hpp' -or -iname '*.h' | xargs ctags --c++-kinds=+p --fields=+iaS --extra=+q --langmap=c++:+.inl -a ~/base_include.tags
+find /usr/include/ -iname '*.cc' -or -iname '*.c' -or -iname '*.cpp' -or -iname '*.hpp' -or -iname '*.h' | xargs ctags --c++-kinds=+p --fields=+iaS --extra=+q --langmap=c++:+.inl -a ~/base_include.tags
+find /usr/local/include/ -iname '*.cc' -or -iname '*.c' -or -iname '*.cpp' -or -iname '*.hpp' -or -iname '*.h' | xargs ctags --c++-kinds=+p --fields=+iaS --extra=+q --langmap=c++:+.inl -a ~/base_include.tags
 
