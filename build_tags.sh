b039b010ce1c6dd67f9d4ec3bd703641fd5542a3
zheng39562 <zheng39562@163.com>
2017-09-30 13:22:13 +0800

    delete useless file.-> build_tags is move to frvim(plugin for vim)

--- build_tags.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-rm ~/base_include.tags
-touch ~/base_include.tags
-find /usr/include -iname "*.c" -or -iname "*.h" -or -iname "*.hpp" -or -iname "*.cpp" | xargs ctags --c++-kinds=+p --fields=+iaS --extra=+q --langmap=c++:+.inl -a -o ~/base_include.tags
-find /usr/local/include -iname "*.c" -or -iname "*.h" -or -iname "*.hpp" -or -iname "*.cpp" | xargs ctags --c++-kinds=+p --fields=+iaS --extra=+q --langmap=c++:+.inl -a -o ~/base_include.tags
-
-exit;
-
--- my_script/build_ctags.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-/usr/bin/rm ~/base_include.tags
-touch ~/base_include.tags
-find /usr/include/ -iname '*.cc' -or -iname '*.c' -or -iname '*.cpp' -or -iname '*.hpp' -or -iname '*.h' | xargs ctags --c++-kinds=+p --fields=+iaS --extra=+q --langmap=c++:+.inl --append=yes -o ~/base_include.tags
-find /usr/local/include/ -iname '*.cc' -or -iname '*.c' -or -iname '*.cpp' -or -iname '*.hpp' -or -iname '*.h' | xargs ctags --c++-kinds=+p --fields=+iaS --extra=+q --langmap=c++:+.inl --append=yes -o ~/base_include.tags
-
